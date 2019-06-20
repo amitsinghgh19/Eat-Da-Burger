1694c4033f014863ed0d48686d376c1b68fd5a9b
amitsinghgh19 <prasadmanigh19>
2019-06-20 14:26:08 -0400

    Committing app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Street Stack", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
